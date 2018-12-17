f2f7ca1f6aa4394261f6a4f8050dc98902c0d5eb
Nauk <cliff1304@gmail.com>
2018-12-17 18:30:54 +0800

    日常練習

---
+++ Ruby/7kyu_missing_values.rb
@@ -0,0 +1,35 @@
+=begin
+題意:
+給一個含有正整數的陣列，每個正整數都有三個
+除了其中一個正整數只出現一次，將此數稱作 x
+另一個正整數只出現兩次，將此數稱作 y
+任務是求 x * x * y
+-----------------------
+腦袋第一個想到的是排序
+不過這題在意的是出現次數，我得找出只出現一次與只出現兩次的數
+我應該用陣列.sort!.each do?
+NO，實測發現修改 each 使用中的陣列會有無法預期的錯誤發生
+用 while 吧，直到找到 x 與 y 為止，都不斷地算個數
+  然後用 count 去數有幾個，如果等於 3 就用 delete 砍掉
+  等於 1 設成 x，宣告後記得要把該元素砍掉
+  等於 2 設成 y
+=end
+def missing_values(array)
+  x = 0
+  y = 0
+  while x == 0 || y == 0
+    p array.sort!
+    count_int = array.count(array[0])
+    case count_int
+    when 1
+      x = array[0]
+    when 2
+      y = array[0]
+    end
+    array.delete(array[0])
+  end
+
+  return (x * x * y)
+end
+
+missing_values([27, 65, 44, 39, 44, 21, 21, 44, 65, 39, 21, 65])
