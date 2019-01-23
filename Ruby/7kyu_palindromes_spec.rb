4c23ea44407d4fd44260f3cb692627793ef5052c
Shih_Kuan <cliff1304@gmail.com>
2019-01-23 20:53:27 +0800

    更新測試規格描述

--- Ruby/7kyu_palindromes_spec.rb
+++ Ruby/7kyu_palindromes_spec.rb
@@ -2,22 +2,27 @@ require_relative './7kyu_palindromes_here_and_there.rb'
 
 RSpec.describe "回文數轉換" do
   it "輸入 [101, 2, 85, 33, 14014] 輸出 [1, 1, 0, 1, 0]" do
-    expect(convert_palindromes([101, 2, 85, 33, 14014])).to eq([1, 1, 0, 1, 0])
+    result = convert_palindromes([101, 2, 85, 33, 14014])
+    expect(result).to eq([1, 1, 0, 1, 0])
   end
 
   it "輸入 [45, 21, 303, 56] 輸出 [0, 0, 1, 0]" do
-    expect(convert_palindromes([45, 21, 303, 56])).to eq([0, 0, 1, 0])
+    result = convert_palindromes([45, 21, 303, 56])
+    expect(result).to eq([0, 0, 1, 0])
   end
 
   it "輸入 [22, 303, 76, 411, 89] 輸出 [1, 1, 0, 0, 0]" do
-    expect(convert_palindromes([22, 303, 76, 411, 89])).to eq([1, 1, 0, 0, 0])
+    result = convert_palindromes([22, 303, 76, 411, 89])
+    expect(result).to eq([1, 1, 0, 0, 0])
   end
 
   it "輸入 [653, 808, 5] 輸出 [0, 1, 1]" do
-    expect(convert_palindromes([653, 808, 5])).to eq([0, 1, 1])
+    result = convert_palindromes([653, 808, 5])
+    expect(result).to eq([0, 1, 1])
   end
 
   it "輸入 [4, 23, 441, 565, 19, 818] 輸出 [1, 0, 0, 1, 0, 1]" do
-    expect(convert_palindromes([4, 23, 441, 565, 19, 818])).to eq([1, 0, 0, 1, 0, 1])
+    result = convert_palindromes([4, 23, 441, 565, 19, 818])
+    expect(result).to eq([1, 0, 0, 1, 0, 1])
   end
 end
